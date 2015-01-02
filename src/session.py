427365c52048322f5a9b9170bd9cb7f3348a2049
Antonin <antonin.carette@gmail.com>
2015-01-02 22:09:51 +0100

    Ajout d'une classe session

---
+++ src/session.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+class session(object):
+
+    """
+    Classe permettant d'initialiser une session FTP
+    """
+
+    def __init__(self, login, mdp, serveur, port = 21):
+        """
+        Constructeur de la session
+        login : login de l'utilisateur
+        mdp : mot-de-passe de la session utilisateur
+        serveur : serveur de la session
+        port : port pour la connexion FTP - par défaut 21
+        """
+        self.login = login
+        self.mdp = mdp
+        self.serveur = serveur
+        self.port = port
+        self.date = datetime.now()
+
+    def __del__(self):
+        """
+        Destructeur de la session
+        """
+        print("Session "+self.serveur+" détruite...")
+
+    def __str__(self):
+        """
+        Impression de l'objet
+        """
+        return self.ret_date()+" :: Session "+self.serveur+" - initialisée par "+self.login
+
+    def ret_date(self):
+        """
+        Fonction permettant de retourner la date d'après l'attribut homonyme
+        """
+        return str(self.date.year)+"/"+str(self.date.month)+"/"+str(self.date.day)+"-"+str(self.date.hour)+":"+str(self.date.minute)+":"+str(self.date.second)
